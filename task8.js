d1a0f40d52aca430eb6a3498108efc1f861a6bc0
viniii05 <vinivt.0520@gmail.com>
2024-08-05 01:59:11 +0530

    saving a meaage into a file

---
+++ task8.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req,res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url === '/message' && method === "POST"){
+        const body = [];
+        req.on('data',(chunks) => {
+            body.push(chunks);
+        })
+
+        req.on('end',() => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+            res.statusCode = 302;
+            res.setHeader('location','/');
+            return res.end();
+        })
+    }
+})
+server.listen(3000);
